d1fa227d4fa70ca419b81d6ee91878d95c0b44ac
lakshya <lakshyaroy848@gmail.com>
2024-06-17 01:46:11 +0530

    new routes created and added to app

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 import Hero from "@/components/Hero";
+import Navbar from "@/components/Navbar";
+import RecentProjects from "@/components/RecentProjects";
+import Overview from "@/components/Overview";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <Navbar />
       <div className="max-w-7xl w-full">
         <Hero />
+        <Overview />
+        <RecentProjects />
       </div>
     </main>
   );
