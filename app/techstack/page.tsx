68c1ca20a1cd0e5c077a3f2f1bf5bfac1f7021d9
Lakshya Roy <lakshyaroy848@gmail.com>
2024-06-25 20:45:08 +0530

    techstack page is created and yet thing to do!

--- app/techstack/page.tsx
+++ app/techstack/page.tsx
@@ -1,7 +1,18 @@
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import Techstacks from "@/components/techstacks";
 import React from "react";
 
 const TechStack = () => {
-  return <div>TechStack</div>;
+  return (
+    <main className="relative w-full bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 pt-5">
+      <Navbar />
+      <div className="max-w-7xl w-full">
+        <Techstacks />
+      </div>
+      <Footer />
+    </main>
+  );
 };
 
 export default TechStack;
---
+++ components/Techstacks.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Techstacks = () => {
+  return <div>techstacks</div>;
+};
+
+export default Techstacks;
